61c44ef2aadf97868ae86aece32e54628c9be452
Javier Garrido <javierationmex@gmail.com>
2015-10-15 12:29:28 -0700

    Added problem 9.3 to chapter 9

---
+++ src/Chapter9/Nine_3.java
@@ -0,0 +1,36 @@
+package Chapter9;
+
+/**
+ * Created by Javier G on 10/15/2015.
+ */
+public class Nine_3 {
+
+    public static void main(String[] args) {
+        int[] array = {-20, -2, 2, 15, 18, 20, 21};
+        System.out.println(magicIndex(array));
+    }
+
+    public static int magicIndex(int[] array){
+        int start = 0;
+        int end = array.length - 1;
+        int solution = helper(start, end, array);
+        return solution;
+    }
+
+    private static int helper(int start, int end, int[] array) {
+
+        int mid = (start + end) / 2;
+        if (mid == array[mid]){
+            return mid;
+        }
+        if (start == end){
+            return -1;
+        }
+        if (array[mid] > mid){
+            return helper(start, mid -1, array);
+        } else {
+            return helper(mid + 1, end, array);
+        }
+    }
+
+}
